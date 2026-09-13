4ba26fe8a07a2ef5839f98d37f59b2922c5aee72
Xroga <evanderthorne.help@gmail.com>
2026-09-14 01:23:07 +0500

    Create index.ts to export software agent modules

---
+++ backend/src/ai/softwareAgent/index.ts
@@ -0,0 +1,14 @@
+export * from './contracts.js';
+export * from './writePolicy.js';
+export * from './runEvents.js';
+export * from './completionGate.js';
+
+export * from './toolHost.js';
+
+export * from './agentModelRoute.js';
+
+export * from './createAgentEvidence.js';
+export * from './softwareAgentPrompt.js';
+export * from './xrogaTools.js';
+
+export * from './AgentSoftwareExecutor.js';
