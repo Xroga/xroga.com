c0fcccc77a9eec06d3df1f067c936c677aa200c7
Xroga <evanderthorne.help@gmail.com>
2026-09-19 15:11:21 +0500

    Create index.ts to export modules from projectRuntime

---
+++ backend/src/synthesis/projectRuntime/index.ts
@@ -0,0 +1,15 @@
+export * from './types.js';
+
+export * from './store.js';
+
+export * from './secretManager.js';
+
+export * from './flyProvider.js';
+
+export * from './manager.js';
+
+export * from './processManager.js';
+
+export * from './dependencyManager.js';
+
+export * from './capabilities.js';
